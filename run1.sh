a11fe1390f165d59478863a2ffddaa3f7f2456e4
tarmg <117362618+tarmg@users.noreply.github.com>
2022-11-23 20:08:15 +0630

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+cd /root
+curl https://rclone.org/install.sh | bash
+pip install rclone
+mkdir /root/.config
+mkdir /root/.config/rclone
+cd /root
+pwd
+git clone https://github.com/tarmg/bot
+cd /root/bot
+cp rclone.conf "/root/.config/rclone/"
+unzip gclone.zip
+unzip AutoRclone.zip
+cd /root/bot/AutoRclone
+pwd
+ls
+python3 rclone_sa_magic.py -s 0ADAKqMg3ZjDyUk9PVA -d 0AFFxLtxsi356Uk9PVA -b 1 -e 100
